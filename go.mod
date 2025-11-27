4dcf501c1e7e4e5b51c2919037e720716da76de4
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-11-26 23:56:59 -0500

    initialize

---
+++ go.mod
@@ -0,0 +1,3 @@
+module google-play-purchases
+
+go 1.21.4
---
+++ purchases.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"archive/zip"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// panics on a non-nil err
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type history []struct {
+	OrderHistory struct {
+		// BillingInstrument interface{}
+		// OtherInstruments  interface{}
+		// BillingContact    interface{}
+		// AssociatedContact interface{}
+		// UserLocale        interface{}
+		OrderId      string
+		CreationTime string
+		IpAddress    string
+		IpCountry    string
+		TotalPrice   string
+		Tax          string
+		RefundAmount string
+		Preorder     bool
+		LineItem     []struct {
+			Doc struct {
+				DocumentType string
+				Title        string
+			}
+			Quantity float64
+		}
+	}
+}
+
+func processFile(file *zip.File) {
+	contents, err := file.Open()
+	check(err)
+	defer contents.Close()
+
+	dec := json.NewDecoder(contents)
+
+	var data history
+	dec.Decode(&data)
+
+	fmt.Println("date, price, item")
+
+	for _, purchase := range data {
+		order := purchase.OrderHistory
+
+		if order.TotalPrice != "$0.00" && order.RefundAmount == "$0.00" {
+			price, err := strconv.ParseFloat(order.TotalPrice[1:], 64)
+			check(err)
+
+			item := ""
+			sep := ""
+			for _, lineItem := range order.LineItem {
+				item += sep + lineItem.Doc.Title
+				sep = ", "
+			}
+
+			date := order.CreationTime
+			dateIndex := strings.Index(order.CreationTime, "T")
+			if dateIndex != -1 {
+				date = order.CreationTime[:dateIndex]
+			}
+
+			fmt.Printf("%s, $%.2f, %s\n", date, price, item)
+		}
+	}
+}
+
+func help() {
+	name := os.Args[0]
+	fmt.Println("a tool for extracting a list of google play purchases")
+	fmt.Println("from a google takeout file\n")
+	fmt.Println("Usage:")
+	fmt.Println(name, "[google takeout zip]")
+	fmt.Println("Example:")
+	fmt.Println(name, "~/Downloads/takeout.zip")
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		help()
+	}
+
+	zipFile, err := zip.OpenReader(os.Args[1])
+	check(err)
+	defer zipFile.Close()
+
+	for _, file := range zipFile.File {
+		if file.Name == "Takeout/Google Play Store/Order History.json" {
+			processFile(file)
+			break
+		}
+	}
+}
